a378af01d73853bd23395407e5336dd5f54dd59c
ardhapanduwinata <ardhapanduwinata@gmail.com>
2020-04-03 16:29:57 +0700

    another tutorial

---
+++ node-mongo/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+// app.use((req, res, next) => {
+//    console.log('ini jalan gak?');
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+module.exports = app;
