5b412f80ad821a4ecd6b3a17b42c9c25927a703c
AM <ahmad.mustofa71@gmail.com>
2020-06-26 14:52:55 +0700

    refactor id

--- app/src/main/java/com/arwinata/am/skripsi/ui/mission/NotificationsViewModel.kt
+++ app/src/main/java/com/arwinata/am/skripsi/ui/mission/NotificationsViewModel.kt
@@ -1,13 +1,13 @@
-package com.arwinata.am.skripsi.ui.dashboard
+package com.arwinata.am.skripsi.ui.mission
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
